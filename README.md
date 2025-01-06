6800284d33f663c5cd4bc571a380d98151b17403
LocNguyenSGU <lockbkbang@gmail.com>
2025-01-06 11:41:44 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Building microservice application
---
+++ permissionservice/src/main/java/com/example/permissionservice/PermissionserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.permissionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermissionserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermissionserviceApplication.class, args);
+	}
+
+}
