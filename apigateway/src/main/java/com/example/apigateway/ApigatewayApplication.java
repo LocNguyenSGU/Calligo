b62d7413a1d46b5e2434a61589da84ae5fd561d2
LocNguyenSGU <lockbkbang@gmail.com>
2025-01-09 12:23:55 +0700

    feat-apigateway: config rate limiting

--- apigateway/src/main/java/com/example/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/example/apigateway/ApigatewayApplication.java
@@ -3,6 +3,9 @@ package com.example.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +14,8 @@ public class ApigatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayApplication.class, args);
 	}
-
+	@Bean
+	KeyResolver userKeyResolver() {
+		return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
+	}
 }
