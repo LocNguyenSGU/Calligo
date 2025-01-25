4fe362d2219428df26ee18aa22b0f75cc1566a41
LocNguyenSGU <lockbkbang@gmail.com>
2025-01-25 16:54:09 +0700

    feat-apigateway: config cors

--- apigateway/src/main/java/com/example/apigateway/config/WebClientConfig.java
+++ apigateway/src/main/java/com/example/apigateway/config/WebClientConfig.java
@@ -4,14 +4,29 @@ import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalance
 import org.springframework.cloud.loadbalancer.config.LoadBalancerCacheAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
 
 @Configuration
 public class WebClientConfig {
-
     @Bean
     public WebClient.Builder loadBalancedWebClientBuilder(ReactorLoadBalancerExchangeFilterFunction loadBalancerFunction) {
         return WebClient.builder()
                 .filter(loadBalancerFunction); // Thêm chức năng load balancer vào WebClient
+    }
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("*"));
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(urlBasedCorsConfigurationSource);
     }
 }
